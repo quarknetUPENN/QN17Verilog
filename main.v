54bdd8ca65e04c2cb41b0cdb2e76831cc8663236
Jonathan Okasinksi <nitrocalcite@users.noreply.github.com>
2017-06-28 17:52:16 -0400

    Frame for two-buffer FPGA data pipe

---
+++ main.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    17:51:03 06/28/2017 
+// Design Name: 
+// Module Name:    main 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module main(
+    input rd_clk
+    );
+
+
+endmodule
